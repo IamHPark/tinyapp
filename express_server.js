d88a29b4c414627071f620b99ad09d1361f11e5f
IamHPark <heesoo.p1023@gmail.com>
2022-04-19 01:51:54 +0000

    Add generateRandomString function

--- express_server.js
+++ express_server.js
@@ -33,6 +33,15 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
+const generateRandomString = function() {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < 6; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+}
 
 
 
