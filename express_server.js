81d9dbe27317604c5699c7f5a9080a0e14ce9b25
IamHPark <heesoo.p1023@gmail.com>
2022-04-19 22:28:04 +0000

    add GET and POST for editing urls

--- express_server.js
+++ express_server.js
@@ -22,35 +22,55 @@ const generateRandomString = function() {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({entended: true}));
 
+// client comes in /urls, show the list of urls(urls_index)
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
+// broswer request "Create New URL" , show create form (urls_new)
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// User fills form and press submit
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
+  //body have a longURL key which is name value in form, update urlDatabase with new URL pair
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
+// redirected from post above, show ulrs_show.ejs
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
-  res.redirect(`/u/${req.params.shortURL}`);
 });
 
+// User press submit then it links to /u/:shortURL, then redirect to longURL
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
+});
+
+// press Edit submit, redirect to urls_show
+app.post("/urls/edit/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  console.log(shortURL);
+  res.redirect(`/urls/${shortURL}`);
 })
 
+// press Delete submit, action to below path, then delete data pair and redirect to /urls
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete(urlDatabase[req.params.shortURL])
   res.redirect("/urls");
-})
+});
+
+// press Edit submit, action to below path, then change longURL in a data pair, redirect to /urls
+app.post("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
