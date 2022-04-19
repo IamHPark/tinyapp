7be5980896955351d3487ecb2a5e2b8398c285b4
IamHPark <heesoo.p1023@gmail.com>
2022-04-19 15:30:06 +0000

    Add a route to redirect to shortURL

--- express_server.js
+++ express_server.js
@@ -34,14 +34,18 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/u/${shortURL}`);
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+  res.redirect(`/u/${req.params.shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
   res.redirect(urlDatabase[req.params.shortURL]);
-});
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
