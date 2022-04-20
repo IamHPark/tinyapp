22d098c8df4ddab69454e57efa24f81e3b957dee
IamHPark <heesoo.p1023@gmail.com>
2022-04-20 05:17:55 +0000

    add login form in /register

--- express_server.js
+++ express_server.js
@@ -24,17 +24,37 @@ const generateRandomString = function() {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({entended: true}));
 
+const users = {};
+// Tiny app register page
+app.get('/register', (req, res) => {
+  const templateVars = { 'username': req.cookies["username"]}
+  res.render('urls_register');
+})
+
+// when user submit login button
+app.post('/register', (req, res) => {
+  const userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password
+  }
+  res
+    .cookie('user_id', userID)
+    .redirect('/urls');
+});
+
 // client comes in /urls, show the list of urls(urls_index)
 app.get('/urls', (req, res) => {
-  const username = req.cookies["username"];
-  const templateVars = { urls: urlDatabase, username: username };
+  const userID = req.cookies["user_id"];
+  const templateVars = { urls: urlDatabase, userID: users[userID] };
   res.render('urls_index', templateVars);
 });
 
 // broswer request "Create New URL" , show create form (urls_new)
 app.get("/urls/new", (req, res) => {
-  const username = req.cookies["username"];
-  const templateVars = { username };
+  const userID = req.cookies["user_id"];
+  const templateVars = { userID: users[userID] };
   res.render("urls_new", templateVars);
 });
 
@@ -48,8 +68,8 @@ app.post("/urls", (req, res) => {
 
 // redirected from post above, show ulrs_show.ejs
 app.get("/urls/:shortURL", (req, res) => {
-  const username = req.cookies["username"];
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username };
+  const userID = req.cookies["user_id"];
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], userID: users[userID] };
   res.render("urls_show", templateVars);
 });
 
@@ -71,6 +91,7 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
 });
 
+/*
 // add endpoint for POST to /login
 app.post("/login", (req, res) => {
   const username = req.body.username;
@@ -79,12 +100,12 @@ app.post("/login", (req, res) => {
     .redirect("/urls");
     // .render("_header" ,username);
 });
+*/
 
 // add endpoint for POST to /logout
 app.post("/logout", (req, res) => {
-  console.log("logout");
   res
-    .clearCookie("username")
+    .clearCookie("user_id")
     .redirect("/urls");
 });
 
